c4f8d9eb0af9eaac6c7076d3496b2d587aca622a
Zachary Hoefler <zachary.hoefler@gmail.com>
2013-05-13 18:54:34 -0400

    Dragging with the left mouse now draws a rect

--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 
 public class UnitManager : MonoBehaviour
 {
-    List<GameObject> units = new List<GameObject>();
+    public Texture2D WhiteTexture;
+    public Color DragRectColor = Color.green;
+    public float DragRectAlpha = 0.5f;
+
+    private List<GameObject> units = new List<GameObject>();
 
     private Vector3 dragStartPos = Vector3.zero;
+    private bool isDragging = false;
 
     void Start()
     {
+        DragRectColor.a = DragRectAlpha;
+
         GameObject[] placedUnits = GameObject.FindGameObjectsWithTag( "Unit" );
         foreach ( GameObject unit in placedUnits ) {
             units.Add( unit );
@@ -20,6 +27,13 @@ public class UnitManager : MonoBehaviour
         HandleLeftMouseDown();
     }
 
+    void OnGUI()
+    {
+        if ( isDragging ) {
+            DrawSelectionBox( dragStartPos, Input.mousePosition );
+        }
+    }
+
     private void HandleLeftMouseDown()
     {
         bool mouseDown = Input.GetMouseButton( 0 );
@@ -27,9 +41,30 @@ public class UnitManager : MonoBehaviour
 
         if ( dragStartedThisFrame ) {
             dragStartPos = Input.mousePosition;
-        } else {
-            Vector3 currentPos = Input.mousePosition;
-
+            isDragging = true;
         }
+
+        if ( isDragging ) {
+            if ( !mouseDown ) {
+                Vector3 dragEndPos = Input.mousePosition;
+                Debug.Log( "Dragged from " + dragStartPos + " to " + dragEndPos );
+                isDragging = false;
+            }
+        }
+    }
+
+    private void DrawSelectionBox( Vector3 start, Vector3 end )
+    {
+        float minX = Mathf.Min( start.x, end.x );
+        float maxX = Mathf.Max( start.x, end.x );
+        float minY = Screen.height - Mathf.Min( start.y, end.y ); // Flip Y to go from Input to GUI coords
+        float maxY = Screen.height - Mathf.Max( start.y, end.y );
+
+        int width = Mathf.FloorToInt( maxX - minX );
+        int height = Mathf.FloorToInt( maxY - minY );
+
+        GUI.color = DragRectColor;
+        GUI.DrawTexture( new Rect( minX, minY, width, height ), WhiteTexture );
+        GUI.color = Color.white;
     }
 }
